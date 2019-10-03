19394cebb6432719b51dbe666160cd5faa2e77fe
Leandro <leandroperezb@gmail.com>
2019-10-03 17:32:18 -0300

    Negativización sólo en constantes (no identificadores)

--- accionessintactico.cpp
+++ accionessintactico.cpp
@@ -2,10 +2,12 @@
 
 void AccionesSintactico::negativizarConstante(TablaSimbolos* tabla, string key){
 	TablaSimbolos::registro* r = &tabla->get(key);
-	(*r).valor = (*r).valor * (-1);
-	(*r).esUlong = false;
-	if ((*r).valor < -32768){
-		cout << "Constante " << (*r).valor << " fuera de rango" << endl;
-		(*r).valor = -32768;
+	if ((*r).palabra != ""){
+		(*r).valor = (*r).valor * (-1);
+		(*r).esUlong = false;
+		if ((*r).valor < -32768){
+			cout << "Constante " << (*r).valor << " fuera de rango" << endl;
+			(*r).valor = -32768;
+		}
 	}
 }
